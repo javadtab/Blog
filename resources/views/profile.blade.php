11ced39e80b95a398a75d135ceb73c19f518b821
javad <sjht@gmail.com>
2024-06-18 12:54:37 +0330

    add profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,9 +1,17 @@
+<!doctype html>
+<html lang="en">
 
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{route('post.profile')}}"><strong>{{ Auth::user()->name }}</strong></a>
+            <a class="navbar-brand" href=""><strong>{{ Auth::user()->name }}</strong></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -31,7 +39,7 @@
         </div>
     </nav>
     <div class="container">
-        <h1><strong> {{ Auth::user()->name}}s information</strong></h1>
+        <h1><strong> {{ Auth::user()->name}} `s information</strong></h1>
         <hr>
         <h2>Name :{{ Auth::user()->name }} </h2>
         <h2>Email Adress :{{ Auth::user()->email }} </h2>
