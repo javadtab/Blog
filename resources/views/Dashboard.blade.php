ac4093f15fc95f96072450774ca1641f61b76709
javad <sjht@gmail.com>
2024-07-09 15:41:25 +0330

    change dash

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -1,3 +1,6 @@
+
+@extends('layouts.app')
+@section('content')
 <!doctype html>
 <html lang="en">
 
@@ -77,3 +80,4 @@
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 </body>
+@endsection
