a0f116d4d1151fb42944b185231c789a92fa255d
javad <sjht@gmail.com>
2024-07-13 12:32:23 +0330

    test authController

---
+++ tests/Feature/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function login_displays_the_login_form()
+{
+    $response = $this->get(route('login'));
+    $response->assertStatus(200);
+    $response->assertViewIs('login');
+}
+}
