84c424e868781abea7d6d35f7901d5e3d3d32a36
javad <sjht@gmail.com>
2024-07-22 17:18:49 +0330

    give style yo post index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container" style="background-color:aqua">
         <div class="titlebar">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
