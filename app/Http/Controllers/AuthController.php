3122e02228a10900a0d5bfab091b5004da4ebd6f
javad <sjht@gmail.com>
2024-08-04 15:40:24 +0330

    d

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
     }
     function profile(Request $request)
     {
-        $ip = $request = 'ip' ;
+        $ip = $request -> user()->ip ;
         $data = Location::get($ip);
 
         $lat = $data->latitude;
