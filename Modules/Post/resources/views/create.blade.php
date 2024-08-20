abe55d793852b98ecde0f073696ac4d0c1cac567
javad <sjht@gmail.com>
2024-08-20 16:55:45 +0330

    repaire post list and create post page

--- Modules/Post/resources/views/create.blade.php
+++ Modules/Post/resources/views/create.blade.php
@@ -6,10 +6,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <h1>Add Post</h1>
+
 </head>
 <body>
     <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <h1>Add Post</h1>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('posts.create') }}">Add Post</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         @csrf
         <hr>
         @if ($errors->any())
