1aabcbaaf0d89ada8a593770240b5ef89a84bc45
javad <sjht@gmail.com>
2024-08-20 12:06:25 +0330

    add post model

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Users\App\Models\User;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Post extends Model implements HasMedia
+{
+    use HasFactory , InteractsWithMedia;
+
+    protected $table = 'posts';
+    protected $fillable = [
+    'user_id',
+    'title',
+    'description',
+   // 'image',
+   ];
+
+   public function user()
+   {
+      return $this->belongsTo(User::class, 'user_id');
+   }
+
+   public function comments()
+   {
+      return $this->hasMany(\Modules\Post\App\Models\Comment::class)->whereNull('parent_id');
+   }
+}
